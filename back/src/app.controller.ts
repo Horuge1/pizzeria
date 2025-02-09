ce7cb9e722749a8983e4f9b8285fc36568fdeabf
horuge <horuge.dev@gmail.com>
2025-02-08 17:21:41 -0700

    Add file upload request

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Express } from 'express';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { join } from 'path';
 
-@Controller()
+@Controller('')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: join(__dirname, '..', 'public/images'),
+        filename: function (req, file, cb) {
+          cb(null, req.body['name'] + '.' + file.originalname.split('.')[1]);
+        },
+      }),
+    }),
+  )
+  uploadFile(
+    @Req() req,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return {imageName:file.filename}
   }
 }
